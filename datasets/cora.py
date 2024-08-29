bbc6efd91cddf2481db2ba3cdad9d3635fa18798
chenyixuan <1406947724@qq.com>
2024-08-29 17:46:01 +0800

    删除无用文件

--- datasets/cora.py
+++ datasets/cora.py
@@ -1,5 +1,4 @@
 import torch
-import os
 from torch_geometric.datasets import Planetoid
 import numpy as np
 import networkx as nx
@@ -7,32 +6,23 @@ from torch_geometric.utils import to_networkx
 from torch.utils import data
 
 
-
-
 class Cora(data.Dataset):
-    def __init__(self, data_type,num_time_steps):
+    def __init__(self, data_type):
         self.load_cora(data_type)
-        self.preprocess_cora(num_time_steps)
-
-    def __len__(self):
-        return len(self.feature_matrix)
-
-    def __getitem__(self, idx):
-        return self.feature_matrix[idx],self.labels[idx]
+        self.preprocess_cora()
 
     def load_cora(self, data_type):
-        assert data_type in ['train', 'test', 'val','all']
         # Load Cora dataset
-        current_path = os.path.dirname(__file__)
-        dataset_cora = Planetoid(root=current_path[0:current_path.rindex('/')]+r'/data', name='Cora')
-        data = dataset_cora[0]
+        dataset_cora = Planetoid(root="../data", name='Cora')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        data = dataset_cora[0].to(device)
         self.feature_matrix = data.x
         self.labels = data.y
         train_mask = data.train_mask
         val_mask = data.val_mask
         test_mask = data.test_mask
         graph = to_networkx(data, to_undirected=True)
-        self.adjacency_matrix = torch.from_numpy(nx.adjacency_matrix(graph).todense())
+        self.adjacency_matrix = torch.from_numpy(nx.adjacency_matrix(graph).todense()).to(device)
         if data_type == 'train':
             self.feature_matrix = self.feature_matrix[train_mask]
             self.labels = self.labels[train_mask]
@@ -51,7 +41,8 @@ class Cora(data.Dataset):
         return
 
     # 添加时序噪声
-    def preprocess_cora(self,num_time_steps):
+    def preprocess_cora(self, num_time_steps=10):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         low, high = -1, 1
         # Initialize list for time-based graphs
         result = []
@@ -62,34 +53,40 @@ class Cora(data.Dataset):
         # 每个值出现的概率
         probabilities = [0.90, 0.05, 0.05]
         for t in range(num_time_steps - 1):
-            noise = torch.from_numpy(np.random.choice(values, p=probabilities, size=self.feature_matrix.shape))
+            noise = torch.from_numpy(np.random.choice(values, p=probabilities, size=self.feature_matrix.shape)).to(
+                device)
             noise_features = result[-1] + noise
 
             # Add features to nodes
             result.append(torch.clamp((noise_features.clone()), min=0, max=1))
 
-        self.feature_matrix = torch.stack(result).permute(1,2,0)
+        self.feature_matrix = torch.stack(result).to(device)
 
 
 if __name__ == '__main__':
 
-    training_set = Cora('train',10)
-    np.savez('../data/Cora/cora_train_set.npz', adjacency_matrix=training_set.adjacency_matrix.cpu().numpy(),
-             feature_matrix=training_set.feature_matrix.cpu().numpy(), labels=training_set.labels.cpu().numpy())
+    a =[]
+    for i in range(20):
+        print(i)
+        a.append(Cora('train').feature_matrix)
+    training_set = torch.stack(a).permute(0,2,1,3)
+
+    np.savez('../data/Cora/cora_train_set.npz', adjacency_matrix=Cora('train').adjacency_matrix.cpu().numpy(),
+             feature_matrix=training_set.cpu().numpy(), labels=Cora('train').labels.cpu().numpy())
 
 
-    val_set = Cora('val',10)
-    np.savez('../data/Cora/cora_val_set.npz', adjacency_matrix=val_set.adjacency_matrix.cpu().numpy(),
-             feature_matrix=val_set.feature_matrix.cpu().numpy(), labels=val_set.labels.cpu().numpy())
-
-
-
-    test_set = Cora('test',10)
-    np.savez('../data/Cora/cora_test_set.npz', adjacency_matrix=test_set.adjacency_matrix.cpu().numpy(),
-             feature_matrix=test_set.feature_matrix.cpu().numpy(), labels=test_set.labels.cpu().numpy())
-
-
-
-    all_set = Cora('all',10)
-    np.savez('../data/Cora/Cora.npz', adjacency_matrix=all_set.adjacency_matrix.cpu().numpy(),
-             feature_matrix=all_set.feature_matrix.cpu().numpy(), labels=all_set.labels.cpu().numpy())
+    # val_set = Cora('val')
+    # np.savez('../data/Cora/cora_val_set.npz', adjacency_matrix=val_set.adjacency_matrix.cpu().numpy(),
+    #          feature_matrix=val_set.feature_matrix.cpu().numpy(), labels=val_set.labels.cpu().numpy())
+    #
+    #
+    #
+    # test_set = Cora('test')
+    # np.savez('../data/Cora/cora_test_set.npz', adjacency_matrix=test_set.adjacency_matrix.cpu().numpy(),
+    #          feature_matrix=test_set.feature_matrix.cpu().numpy(), labels=test_set.labels.cpu().numpy())
+    #
+    #
+    #
+    # all_set = Cora('all')
+    # np.savez('../data/Cora/Cora.npz', adjacency_matrix=all_set.adjacency_matrix.cpu().numpy(),
+    #          feature_matrix=all_set.feature_matrix.cpu().numpy(), labels=all_set.labels.cpu().numpy())
